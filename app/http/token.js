7a6a42339c0637d4fe2acb071ba5d35ad292f7f1
Jared Hanson <jaredhanson@gmail.com>
2017-06-07 17:49:54 -0700

    Reorganize token service.

--- app/http/token.js
+++ app/http/token.js
@@ -1,15 +1,15 @@
-exports = module.exports = function(server, verifyCb, issueCb) {
-  return server.accessToken(verifyCb, issueCb);
+exports = module.exports = function(initiateHandler, tokenHandler) {
+  var express = require('express');
+  var router = new express.Router();
+  
+  router.get('/initiate', initiateHandler);
+  router.get('/token', tokenHandler);
+  
+  return router;
 };
 
-
+exports['@implements'] = 'http://schemas.authnomicon.org/js/oauth/http/TokenService';
 exports['@require'] = [
-  'http://schemas.modulate.io/js/aaa/oauth/Server',
-  './_token/verifycb',
-  './_token/issuecb'
-];
-
-exports['@implements'] = [
-  'http://i.expressjs.com/endpoint',
-  'http://schemas.modulate.io/js/aaa/oauth/endpoint/token'
+  './initiate/initiate',
+  './token/token'
 ];
